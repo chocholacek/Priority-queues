3495d9ae035de5f9f328fbb95554a31d3dd3dc0f
Marek Chocholacek <xchoho2@fi.muni.cz>
2019-05-07 14:48:48 +0200

    violation heap consolidate change

--- src/ViolationHeap/violation_heap.hpp
+++ src/ViolationHeap/violation_heap.hpp
@@ -2,11 +2,11 @@
 
 #include <cmath>
 #include <functional>
+#include <memory>
+#include <vector>
 #include "../base/HeapBase.hpp"
 
 namespace MC {
-
-static int bin = 0;
 
 template<typename Item>
 class ViolationHeap : public HeapBase {
@@ -24,107 +24,100 @@ protected:
 
         int rank = 0;
 
-        Node(int k, const Item& i) : key(k), item(i) {
-            AssignSelf();
-        }
+        Node(int k, const Item& i) : key(k), item(i) {}
 
-
-
-    private:
-        Node* GetLast(std::function< void(Node*) > action = [](Node*){}, int& count = bin) {
-            auto curr = this;
-            while (curr->next != this) {
-                action(curr);
-                ++count;
-                curr = curr->next;
-            }
-            return curr;
-        }
-
-        void Unlink() {
-            next = prev = nullptr;
-        }
-
-        void Link(Node* n) {
+        void AddChild(Node* n) {
             auto c = child;
-
-            child = n;
             n->prev = this;
+            n->next = child;
+            child = n;
 
-            n->next = c;
             if (c)
                 c->prev = n;
         }
 
-        void AssignSelf() {
-            next = this;
+        Node* ActiveChildWithLargetsRank() const {
+            if (!child)
+                return nullptr;
+
+            int r1 = child->rank;
+            if (!child->next)
+                return child;
+            int r2 = child->next->rank;
+
+            return r1 > r2 ? child : child->next;
         }
 
         bool IsRoot() const {
             return prev == nullptr;
         }
 
-        // returns parent if this node is active
-        // nullptr otherwise
-        Node* Active() const {
-            auto curr = prev;
-
-            if (this == curr->child)
-                return curr;
-
-            curr = curr->prev;
-
-            if (this == curr->child)
-                return curr;
-
+        // returns pointer to parent if node is active
+        static Node* IsActive(Node* which) {
+            if (which->IsRoot())
+                return nullptr;
+            if (which == which->prev->child)
+                return which->prev;
+            if (which == which->prev->prev->child)
+                return which->prev->prev;
             return nullptr;
         }
 
-        // recalculates rank and returns old rank
+        void Replace(Node* by) {
+            auto byp = by->prev;
+            auto byn = by->next;
+
+            by->next = next;
+            by->prev = prev;
+            if (next)
+                next->prev = by;
+            if (this == prev->child) { // this is 1st child
+                prev->child = by;
+            } else {
+                prev->next = by;
+            }
+
+            if (child == by) { // by is first child
+                child = byn;
+                if (byn)
+                    byn->prev = this;
+            } else {
+                child->next = byn;
+                if (child->next)
+                    child->next->prev = byp;
+            }
+        }
+
+        // returns old rank
         int RecalculateRank() {
-            if (!child)
-                return rank;
-
-            auto c2 = child->next;
-
-            float rc1 = child->rank;
-            float rc2 = c2 ? c2->rank : -1;
-
-            int r = rank;
-            rank = static_cast< int >(std::ceil((rc1 + rc2) / 2) + 1);
-            return r;
+            int o = rank;
+            int r1 = child ? child->rank : -1;
+            int r2 = -1;
+            if (child && child->next)
+                r2 = child->next->rank;
+            rank = std::ceil((r1 + r2) / 2) + 1;
+            return o;
         }
 
-        Node* LargestActiveChild() const {
-            if (!child)
-                return nullptr;
-
-            int rc1 = child->rank;
-
-            if (!child->next)
-                return child;
-
-            int rc2 = child->next->rank;
-
-            return rc2 > rc1 ? child->next : child;
-        }
-
-        void Free() {
+        ~Node() {
             while (child) {
-                child->Free();
                 auto n = child->next;
                 delete child;
                 child = n;
             }
         }
+
     };
 
     Node* roots = nullptr;
+    std::size_t count = 0;
 public:
 
     using NodeType = ViolationHeap::Node;
 
     ViolationHeap() : HeapBase("Violation heap") {};
+
+    bool Empty() const { return count == 0; }
 
     const Node& Min() const {
         if (!roots)
@@ -135,7 +128,14 @@ public:
 
     const Node* Insert(int key, const Item& item) {
         auto n = new Node(key, item);
-        Meld(n);
+        n->next = n;
+        ++count;
+
+        AddToRoots(n);
+
+        if (n->key < roots->key)
+            roots = n;
+
         return n;
     }
 
@@ -145,68 +145,75 @@ public:
 
         auto n = const_cast< Node* >(node);
         n->key = key;
+//        throw;
 
+        // if is root stop; if key is smaller make it new min
         if (n->IsRoot()) {
             if (n->key < roots->key)
                 roots = n;
             return;
         }
 
-        Node* p = nullptr;
+        Node* parent = nullptr;
 
-        if ((p = n->Active()) && p->key <= n->key)
+        // If n is an active node whose new value is not smaller than its parent, stop.
+        if ((parent = Node::IsActive(n)) && n->key >= parent->key) {
             return;
+        }
 
+        auto lc = n->ActiveChildWithLargetsRank();
+        auto curr = n;
+        if (lc) {
+            n->Replace(lc);
+            curr = lc;
+        } else {
+            if (parent && n == parent->child) {
+                parent->child = n->next;
+            } else {
+                n->prev->next = n->next;
+            }
+
+            if (n->next) {
+                curr = n->next->prev = n->prev;
+            } else {
+                curr = parent;
+            }
+        }
 
         int oldRank = n->RecalculateRank();
-        auto curr = n;
-        while (!curr->IsRoot() && oldRank > curr->rank && (p = curr->Active())) {
-            oldRank = p->RecalculateRank();
-            curr = p;
+
+
+        while (curr && curr->rank < oldRank && (parent = Node::IsActive(curr))) {
+            oldRank = parent->RecalculateRank();
+            curr = parent;
         }
 
-        auto lc = n->LargestActiveChild();
+        n->prev = nullptr;
+        n->next = n;
 
-        Cut(lc);
-        lc->next = n->next;
-        lc->prev = n->prev;
-
-        // n is first child
-        if (n == n->prev->child) {
-            n->prev->child = lc;
-
-        } else {
-            n->prev->next = lc;
-        }
-
-        if (n->next)
-            n->next->prev = lc;
-
-        n->AssignSelf();
-        Meld(n);
-
+        AddToRoots(n);
+        if (n->key < roots->key)
+            roots = n;
     }
 
-    Item ExtractMin() {
+    std::unique_ptr< Node > ExtractMin() {
         if (!roots)
             EmptyException();
 
         auto min = roots;
-        auto children = min->child;
-        min->child = nullptr;
-        Item i = min->item;
+        std::unique_ptr< Node > ret(min);
+        PromoteChildren(min);
 
-        roots = roots->next;
-        delete min;
-
-        if (roots != min) {
-            Meld(children);
-            Consolidate(min);
-        } else {
+        --count;
+        if (count == 0) {
             roots = nullptr;
+            return ret;
         }
 
-        return i;
+        roots = roots->next;
+        Consolidate(min);
+
+        return ret;
     }
 
     ~ViolationHeap() {
@@ -215,100 +222,110 @@ public:
 
 private:
 
-    void Cut(Node* n) {
-        // n is first child
-        if (n == n->prev->child) {
-            n->prev->child = n->next;
-
-        } else {
-            n->prev->next = n->next;
-        }
-
-        if (n->next)
-            n->next->prev = n->prev;
-
-    }
-
-    void Meld(Node* h) {
-        if (!h)
-            return;
-
+    void AddToRoots(Node* n) {
         if (!roots) {
-            roots = h;
+            roots = n;
             return;
         }
 
-        if (h->key < roots->key)
-            std::swap(roots,h);
+        n->prev = nullptr;
+        auto act = n;
 
-        auto next = roots->next;
-        roots->next = h;
-        auto curr = h->GetLast([](auto* n) { n->prev = nullptr; });
+        while (act->next && act->next != n) {
+            act->prev = nullptr;
+            act = act->next;
+        }
 
-        curr->next = next;
+        act->next = roots->next;
+        roots->next = act;
     }
 
-    void DeleteRoots() {
-        if (!roots)
-            return;
+    void AddToRoots(const std::vector< Node* > nodes) {
+        for (auto n : nodes) {
+            n->prev = nullptr;
+            n->next = n;
+            AddToRoots(n);
+            if (n->key < roots->key)
+                roots = n;
+        }
+    }
 
-        auto n = roots;
-        do {
-            auto m = n->next;
-            n->Free();
-            delete n;
-            n = m;
-        } while (n != roots);
+    void PromoteChildren(Node* n) {
+        auto child = n->child;
+
+        while (child) {
+            auto next = child->next;
+
+            child->prev = nullptr;
+            child->next = child;
+            AddToRoots(child);
+
+            child = next;
+        }
+
+        n->child = nullptr;
     }
 
     void Consolidate(Node* end) {
-        if (!roots)
-            return;
+        int bound = std::ceil(std::log2(count)) + 1;
+        int mr = 0;
+//        std::vector< std::vector< Node* > > newRoots(bound);
+        
+        std::vector< Node* > newRoots(bound);
+        std::vector< Node* > tmp(count * 2);
 
-        std::vector< std::vector< Node* >> ranks(64);
-
-        auto z = roots;
+        auto act = roots;
         do {
-            auto r = z->rank;
+            auto next = act->next;
 
-            while (ranks[r].size() == 2) {
-                auto& v = ranks[r];
-                auto z1 = v.back();
-                v.pop_back();
-                auto z2 = v.back();
-                v.pop_back();
+            auto z = act;
+            int r = z->rank;
 
-                if (z1->key < z->key)
-                    std::swap(z1, z);
+            while (newRoots[r].size() == 2) {
+                auto& v = newRoots[r];
+                auto z1 = v[0];
+                auto z2 = v[1];
 
-                if (z2->key < z->key)
-                    std::swap(z2, z);
+                v.clear();
 
-                z->Link(z1);
-                z->Link(z2);
+                if (z->key > z1->key)
+                    std::swap(z, z1);
 
+                if (z->key > z2->key)
+                    std::swap(z, z2);
+
+                z->AddChild(z1);
+                z->AddChild(z2);
                 r = ++z->rank;
             }
 
-            ranks[r].push_back(z);
-            auto next = z->next;
+            newRoots[r].push_back(z);
+            act = next;
 
-            z->Unlink();
-
-            z = next;
-        } while (z != end);
+            if (z->rank > mr)
+                mr = z->rank;
+        } while (act != end);
 
         roots = nullptr;
-        for (auto& v : ranks) {
-            while (v.size()) {
-                auto n = v.back();
-                n->AssignSelf();
-                Meld(n);
-                v.pop_back();
-            }
+
+        for (int i = 0; i <= mr; ++i) {
+            auto& v = newRoots[i];
+            AddToRoots(v);
         }
 
     }
+
+    void DeleteRoots() {
+        if (roots) {
+            auto act = roots;
+            do {
+                auto next = act->next;
+                delete act;
+                act = next;
+            } while (act != roots);
+        }
+    }
+
 };
 
 
